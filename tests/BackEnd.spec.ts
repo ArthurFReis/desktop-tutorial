e8fa37345afcf162427ffa948998e1b6f12517f1
ArthurFReis <arfure21@gmail.com>
2026-05-12 08:23:13 -0300

    Backend 3

--- tests/BackEnd.spec.ts
+++ tests/BackEnd.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe.parallel("API com paralelo", () => {
 
     test("tipos de teste de backend", async({request, page}) => {
-        let tiposTest = [1,2,3,4];
+        let tiposTest = [1,2,3];
         const site = 'https://brasilapi.com.br/api';
 
         for(let i = 0; i < tiposTest.length; i++){
@@ -13,15 +13,13 @@ test.describe.parallel("API com paralelo", () => {
                 const response = await request.get(`${site}/banks/v1`);
                 expect(response.status()).toBe(200);
                 break;
+           
             case 2:
-                const response2 = await request.get(`${site}/banks/v1`);
-                //expect(response2.status()).toBe(400);
-                break;
-            case 3:
                 const response3 = await request.get(`${site}/banks/v1/1`);
                 console.log(response3.status());
                 expect(response3.status()).toBe(200);
-                break;  
+                break; 
+
             case 4:
                 const response4 = await request.get(`${site}/cep/v1/89010025`);
                 expect(response4.status()).toBe(200);
@@ -36,6 +34,12 @@ test.describe.parallel("API com paralelo", () => {
         
     }
 
+    });
+
+    test("Bancos em geral a validação errada", async({request , page}) => {
+        const site = 'https://brasilapi.com.br/api';
+        const response = await request.get(`${site}/banks/v1`);
+        expect(response.status()).toBe(400);
     });
 
 });
