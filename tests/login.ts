ad5386c05196873c830951d010b785d813bb2f0a
ArthurFReis <arfure21@gmail.com>
2026-05-18 10:52:18 -0300

    alignment

--- tests/login.ts
+++ tests/login.ts
@@ -1,21 +1,32 @@
-import { Page, expect } from '@playwright/test';
+import { Page, expect } from "@playwright/test";
 
-
-
-export async function pageResponsivoLogin(page: Page,) {
-    let tamanhos = {"nomes": ["iPhone_SE", "iPhone_XR", "iPhone_12_Pro", "iPhone_14_Pro_Max"], "width": [375,414,390,430], "height": [667,896,844,932]};
-    for(const tamanho in tamanhos.width){
-      if (page.url() === 'https://www.saucedemo.com/') {
-      await page.setViewportSize({ width: tamanhos.width[tamanho], height: tamanhos.height[tamanho]});
-      await page.screenshot({path: `Evidencias/login/Responsivo/Saucedemo/mobile-${tamanhos.nomes[tamanho]}.png`});
+export async function pageResponsivoLogin(page: Page) {
+  let tamanhos = {
+    nomes: ["iPhone_SE", "iPhone_XR", "iPhone_12_Pro", "iPhone_14_Pro_Max"],
+    width: [375, 414, 390, 430],
+    height: [667, 896, 844, 932],
+  };
+  for (const tamanho in tamanhos.width) {
+    if (page.url() === "https://www.saucedemo.com/") {
+      await page.setViewportSize({
+        width: tamanhos.width[tamanho],
+        height: tamanhos.height[tamanho],
+      });
+      await page.screenshot({
+        path: `Evidencias/login/Responsivo/Saucedemo/mobile-${tamanhos.nomes[tamanho]}.png`,
+      });
+    } else {
+      await page.setViewportSize({
+        width: tamanhos.width[tamanho],
+        height: tamanhos.height[tamanho],
+      });
+      await page.screenshot({
+        path: `Evidencias/login/Responsivo/Inventory/mobile-${tamanhos.nomes[tamanho]}.png`,
+      });
     }
-    else {
-      await page.setViewportSize({ width: tamanhos.width[tamanho], height: tamanhos.height[tamanho]});
-      await page.screenshot({path: `Evidencias/login/Responsivo/Inventory/mobile-${tamanhos.nomes[tamanho]}.png`});
-    }
-     console.log("É responsivo! \n");
-     console.log("======================================================================================================================== \n");
-    
+    console.log("É responsivo! \n");
+    console.log(
+      "======================================================================================================================== \n",
+    );
   }
-
 }
