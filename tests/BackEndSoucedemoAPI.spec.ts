ebfaa1763f019e1c4b4580a53379a3faa14e5a0c
ArthurFReis <arfure21@gmail.com>
2026-05-20 06:49:30 -0300

    Novo BackEnd

---
+++ tests/BackEndSoucedemoAPI.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.parallel("API com paralelo", () => {
+  test("Login correto metodo errado", async ({ request, page }) => {;
+    const response = await request.post(`https://www.saucedemo.com/`, {
+      form: {
+        Username: "standard_user",
+        Password: "secret_sauce",
+      },
+    });
+    expect(response.status()).toBe(200);
+    console.log("Login correto, status: ", response.status());
+  });
+
+   test("Login correto", async ({ request, page }) => {;
+    const response = await request.get(`https://www.saucedemo.com/`, {
+      form: {
+        Username: "standard_user",
+        Password: "secret_sauce",
+      },
+    });
+    expect(response.status()).toBe(200);
+    console.log("Login correto, status: ", response.status());
+  });
+
+  test("Login Erro de status", async ({ request, page }) => {;
+    const response = await request.get(`https://www.saucedemo.com/`, {
+      form: {
+        Username: "standard_user",
+        Password: "secret_sauce",
+      },
+    });
+    expect(response.status()).toBe(400);
+    console.log("Login erro de status, status: ", response.status());
+  });
+
+  test("Login erro de usuário", async ({ request, page }) => {;
+    const response = await request.get(`https://www.saucedemo.com/`, {
+      form: {
+        Username: "standard_user1",
+        Password: "secret_sauce",
+      },
+    });
+    expect(response.status()).toBe(403);
+    console.log("Login erro de usuário, status: ", response.status());
+  });
+
+  test("Login erro de password", async ({ request, page }) => {;
+    const response = await request.get(`https://www.saucedemo.com/`, {
+      form: {
+        Username: "standard_user",
+        Password: "secret_sauce1",
+      },
+    });
+    expect(response.status()).toBe(403);
+    console.log("Login erro de password, status: ", response.status());
+  });
+
+});
